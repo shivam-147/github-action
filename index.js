87b0f11a3d7646eb3cdd678cb7bb506d10ef7532
shivam <shivam.bathre@reak.in>
2026-03-08 17:48:52 +0530

    welcome

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = 8080
 
 app.get('/', (req, res) => {
-    res.json({ msg: 'Hello world from the server setting up the cd and cd' })
+    res.json({ msg: 'welcome to ci and cd from shivam bathre' })
 })
 
 
